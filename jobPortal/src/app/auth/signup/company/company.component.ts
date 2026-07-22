d21b3a9cf4d090446bfa48c9c16b004a1083ba31
Naveen <naveensanthosh2019@gmail.com>
2026-07-22 11:46:20 +0530

    company-seeker'

--- jobPortal/src/app/auth/signup/company/company.component.ts
+++ jobPortal/src/app/auth/signup/company/company.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-company',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./company.component.css']
 })
 export class CompanyComponent {
+constructor(private route : Router){}
+
+login(){
+  this.route.navigate(['/login']);
+}
+ signUp(){
+  this.route.navigate(['/register']);
+}
 
 }
--- jobPortal/src/app/auth/signup/seeker/seeker.component.ts
+++ jobPortal/src/app/auth/signup/seeker/seeker.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-seeker',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./seeker.component.css']
 })
 export class SeekerComponent {
+  constructor(private route : Router){}
 
+  signUp(){
+  this.route.navigate(['/register']);
+}
+
+login(){
+  this.route.navigate(['/login']);
+}
 }
