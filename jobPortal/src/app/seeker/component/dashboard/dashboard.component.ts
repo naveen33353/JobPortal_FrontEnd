697b2ff529aa7d4e23fcbc428fcccb0394053a7b
Abijith_ps <abijithps2003@gmail.com>
2026-07-27 11:15:58 +0530

    restart

--- jobPortal/src/app/seeker/component/dashboard/dashboard.component.ts
+++ jobPortal/src/app/seeker/component/dashboard/dashboard.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
 import { jobSeeker } from 'src/app/auth/Models/jobSeeker';
-<<<<<<< HEAD
 import { AuthService } from 'src/app/service/auth.service';
 import { JobService } from 'src/app/service/job/job.service';
 import { JobseekerService } from 'src/app/service/jobseeker/jobseeker.service';
-=======
 import { SeekerService } from 'src/app/service/seeker/seeker.service';
->>>>>>> a49905d857a6c2aeb02ed8ee2f7ad42c176610c4
 
 @Component({
   selector: 'app-dashboard',
@@ -14,31 +11,9 @@ import { SeekerService } from 'src/app/service/seeker/seeker.service';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
-constructor(private service : JobseekerService){}
 
-<<<<<<< HEAD
 firstName! : string;
 
-getjobSeekerById(){
-  const id = Number(localStorage.getItem("id"));
-
-  this.service.getJobSeekerById(id).subscribe(
-    (res) => {
-      console.log(res);
-      
-      this.firstName = res.firstName;
-
-      if(this.firstName == ""){
-         console.log('Firstname is empty');
-      }
-
-      console.log("First Name:", this.firstName);
-    },
-    (error) => {
-      console.log(error);
-    }
-  );
-=======
   constructor(private seekerService:SeekerService){}
 
   id: number = Number(localStorage.getItem("Id"));
@@ -60,13 +35,4 @@ getjobSeekerById(){
     error: (err) => console.log(err)
   });
 }
->>>>>>> a49905d857a6c2aeb02ed8ee2f7ad42c176610c4
-}
-
-ngOnInit(){
-  this.getjobSeekerById();
-}
-
-
-
 }
