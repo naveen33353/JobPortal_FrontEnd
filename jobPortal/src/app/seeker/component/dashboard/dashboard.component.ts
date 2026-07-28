3b3d94d82267303fec7e79d8c3a982434a0a7836
Naveen <naveensanthosh2019@gmail.com>
2026-07-28 10:50:56 +0530

    resolved

--- jobPortal/src/app/seeker/component/dashboard/dashboard.component.ts
+++ jobPortal/src/app/seeker/component/dashboard/dashboard.component.ts
@@ -2,9 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { jobSeeker } from 'src/app/auth/Models/jobSeeker';
 import { ApplicationService } from 'src/app/service/application/application.service';
-import { AuthService } from 'src/app/service/auth.service';
-import { JobService } from 'src/app/service/job/job.service';
-import { JobseekerService } from 'src/app/service/jobseeker/jobseeker.service';
+
 import { SeekerService } from 'src/app/service/seeker/seeker.service';
 
 @Component({
@@ -16,15 +14,17 @@ export class DashboardComponent {
 
 // firstName! : string;
 
-<<<<<<< HEAD
-  constructor(private seekerService:SeekerService , private router:Router){}
 
-  id: number = Number(localStorage.getItem("id"));
-=======
-  constructor(private seekerService : SeekerService, private applicationService : ApplicationService){}
+  
+
+  constructor(private seekerService : SeekerService, private applicationService : ApplicationService,
+               private router : Router){}
 
   id : number = Number(localStorage.getItem("id"));
->>>>>>> 8c74e6d (28-07-26 morning)
+
+
+ 
+
 
  
 
@@ -41,12 +41,12 @@ export class DashboardComponent {
   });
 }
 
-<<<<<<< HEAD
+
 logout() {
   localStorage.clear();
   this.router.navigate(['/']);
 }
-=======
+
 applicationsById :  any[] =[];
  totalApplications = 0;
 
@@ -70,5 +70,9 @@ getApplicationsByJobseekerId(id : number){
     this.getApplicationsByJobseekerId(this.id);
     console.log(this.totalApplications);
   }
->>>>>>> 8c74e6d (28-07-26 morning)
+
+
+
+
+
 }
